b6705efdced74edc492ad4085deeca63c7f2bd4d
MonkeyBean <monkeybean_zhang@163.com>
2018-09-25 18:54:08 +0800

    新增跨域声明字段

--- BootLabo/src/main/java/com/monkeybean/labo/component/filter/CrossOriginFilter.java
+++ BootLabo/src/main/java/com/monkeybean/labo/component/filter/CrossOriginFilter.java
@@ -20,7 +20,18 @@ public class CrossOriginFilter implements Filter {
             throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        //接收任意域名请求
         httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+
+        //允许任意请求方法
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "*");
+
+        //允许浏览器发送cookie
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+
+        //列出浏览器CORS请求会额外发送的头字段，若用于签名校验的stime或sign放在头部，则需声明
+//        httpServletResponse.setHeader("Access-Control-Allow-Headers", "stime,sign");
         chain.doFilter(httpServletRequest, httpServletResponse);
     }
 
