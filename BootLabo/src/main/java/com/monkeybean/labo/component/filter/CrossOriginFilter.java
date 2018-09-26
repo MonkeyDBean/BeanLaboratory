e7baafb03fef3006926dc2a93b9000fbb5d28453
MonkeyBean <monkeybean_zhang@163.com>
2018-09-26 17:44:32 +0800

    新增跨域说明

--- BootLabo/src/main/java/com/monkeybean/labo/component/filter/CrossOriginFilter.java
+++ BootLabo/src/main/java/com/monkeybean/labo/component/filter/CrossOriginFilter.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
+ * 若使用session策略作为用户身份区分，建议前后端为同一域名下，通过nginx路径匹配，反向代理到后端，避免跨域问题的出现
+ * <p>
  * Created by MonkeyBean on 2018/05/26.
  */
 public class CrossOriginFilter implements Filter {
@@ -20,20 +22,34 @@ public class CrossOriginFilter implements Filter {
             throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-
-        //接收任意域名请求
         httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
-
-        //允许任意请求方法
-        httpServletResponse.setHeader("Access-Control-Allow-Methods", "*");
-
-        //允许浏览器发送cookie
-        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
-
-        //列出浏览器CORS请求会额外发送的头字段，若用于签名校验的stime或sign放在头部，则需声明
-//        httpServletResponse.setHeader("Access-Control-Allow-Headers", "stime,sign");
         chain.doFilter(httpServletRequest, httpServletResponse);
     }
+
+//    /**
+//     * 允许跨域
+//     */
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//            throws IOException, ServletException {
+//        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+//        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+//
+//        //若前后端为不同域名，会有Session跨域问题，同一用户的SessionId不一致。服务端如下设置，允许跨域；同时前端设置withCredentials为true，解决浏览器的同源限制
+//        //可参照：https://blog.csdn.net/weixin_40461281/article/details/81196932
+//        //接收任意域名请求
+//        httpServletResponse.setHeader("Access-Control-Allow-Origin", httpServletRequest.getHeader("Origin"));
+//
+//        //允许任意请求方法
+//        httpServletResponse.setHeader("Access-Control-Allow-Methods", "*");
+//
+//        //允许浏览器发送cookie
+//        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+//
+//        //列出浏览器CORS请求会额外发送的头字段，若用于签名校验的stime或sign放在头部，则需声明
+////        httpServletResponse.setHeader("Access-Control-Allow-Headers", "stime,sign");
+//        chain.doFilter(httpServletRequest, httpServletResponse);
+//    }
 
     @Override
     public void init(FilterConfig arg0) throws ServletException {
