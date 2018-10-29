ecfd933264e2efdad2cb53dbf12415dd44c4693a
MonkeyBean <monkeybean_zhang@163.com>
2018-10-29 14:13:15 +0800

    新增文件读取测试

--- DataAlgorithm/src/test/java/com/monkeybean/algorithm/category/OtherUtilTest.java
+++ DataAlgorithm/src/test/java/com/monkeybean/algorithm/category/OtherUtilTest.java
@@ -2,9 +2,13 @@ package com.monkeybean.algorithm.category;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileStore;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 /**
  * Created by MonkeyBean on 2018/8/6.
@@ -25,7 +29,7 @@ public class OtherUtilTest {
         OtherUtil.printTwoHeart();
     }
 
-    @Test
+    //    @Test
     public void testMapStream() {
         List<Integer> origin = new ArrayList<>();
         for (int i = 1; i <= 10; i++) {
@@ -40,11 +44,34 @@ public class OtherUtilTest {
         System.out.println("sum is: " + origin.stream().mapToInt(e -> e).distinct().sum());
     }
 
-    @Test
+    //    @Test
     public void testForceCast() {
         Object b = null;
         Integer value = (Integer) b;
         System.out.println("testForceCast, value: " + value);
+    }
+
+//    @Test
+    public void testFileRead() throws Exception {
+
+        //列出文件
+        String desktopPathStr = "C:\\Users\\Administrator\\Desktop\\";
+        FileStore desktopStore = Files.getFileStore(Paths.get(desktopPathStr));
+        System.out.println("Desktop总空间：" + desktopStore.getTotalSpace() / (1024 * 1024.0) + "MB");
+        System.out.println("Desktop可用空间：" + desktopStore.getUsableSpace() / (1024 * 1024.0) + "MB");
+        System.out.println("Desktop所有文件：");
+        Files.list(Paths.get(desktopPathStr)).forEach(System.out::println);
+
+        //复制文件
+        String filePathStr = "C:\\Users\\Administrator\\Desktop\\test_file\\testFile.txt";
+        String newFilePathStr = "C:\\Users\\Administrator\\Desktop\\test_file\\testFile2.txt";
+        Path path = Paths.get(filePathStr);
+        Files.copy(path, new FileOutputStream(newFilePathStr));
+        System.out.println("filePath, 是否为隐藏文件：" + Files.isHidden(path));
+
+        //读取文件
+        Map<String, String> fileContentMap = new HashMap<>();
+        Files.lines(Paths.get(newFilePathStr), StandardCharsets.UTF_8).forEach(value -> fileContentMap.put(path.getFileName().toString(), value));
     }
 
 }
