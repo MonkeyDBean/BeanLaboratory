96b2c9d7b36b4be2ccd7ae39f5371e4dc9549757
MonkeyBean <monkeybean_zhang@163.com>
2018-11-02 12:19:50 +0800

    新增list合并去重和list转换测试

--- BootLabo/src/test/java/com/monkeybean/labo/util/OtherTest.java
+++ BootLabo/src/test/java/com/monkeybean/labo/util/OtherTest.java
@@ -2,9 +2,8 @@ package com.monkeybean.labo.util;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by MonkeyBean on 2018/7/26.
@@ -16,7 +15,7 @@ public class OtherTest {
         Integer.valueOf(null);
     }
 
-    @Test
+    //    @Test
     public void testLinkedList() {
         LinkedList<String> list = new LinkedList<>();
         list.add("111");
@@ -42,10 +41,43 @@ public class OtherTest {
             System.out.println("element" + i + ": " + list.get(i));
         }
     }
-    
-    @Test
+
+    //    @Test
     public void testSplit() {
         String testStr = "test";
         System.out.println("split 0 is: \n" + testStr.split(",")[0]);
+    }
+
+    @Test
+    public void testMergeList() {
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(4);
+        list2.add(5);
+        list2.add(6);
+        list1.removeAll(list2);
+        list1.addAll(list2);
+        System.out.println("list1Result: " + Arrays.toString(list1.toArray()));
+
+        // 注：通过Arrays.asList()转换的List不可调用addAll()等方法，否则会报UnsupportedOperationException
+        // 原因是调用Arrays.asList()方法生成List时返回的是Arrays的静态内部类ArrayList(此ArrayList不是util包下的ArrayList),
+        // 它自身并未重写addAll()等方法，而其父类AbstractList实现的addAll()等方法只会抛出UnsupportedOperationException
+        // 如果仍想使用addAll()等方法，则用其他方式转换，不可用Arrays.asList()
+        List<String> list3 = Arrays.asList("a", "b", "c", "d");
+        String list4Str = "a,b ,e ,f";
+        List<String> list4 = Arrays.stream(list4Str.split(",")).map(s -> (s.trim())).collect(Collectors.toList());
+
+        //报UnsupportedOperationException
+//        list3.addAll(list4);
+
+        list4.addAll(list3);
+        list4 = new ArrayList<>(new HashSet<>(list4));
+        String list4ResultStr = list4.stream().collect(Collectors.joining(","));
+        System.out.println("list4Result: " + list4ResultStr);
     }
 }
