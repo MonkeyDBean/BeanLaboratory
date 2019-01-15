83d4eb1b840f0f02c6c479c56a27a9695177da6c
MonkeyDBean <monkeybean_zhang@163.com>
2019-01-15 11:53:46 +0800

    新增测试类事务及回滚

--- BootLabo/src/test/java/com/monkeybean/labo/service/PublicServiceTest.java
+++ BootLabo/src/test/java/com/monkeybean/labo/service/PublicServiceTest.java
@@ -6,13 +6,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by MonkeyBean on 2018/7/26.
  */
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@Transactional
 public class PublicServiceTest {
 
     private static Logger logger = LoggerFactory.getLogger(PublicServiceTest.class);
