b9180a0947120e5f9cba923df4b41bb7226d8469
MonkeyDBean <monkeybean_zhang@163.com>
2019-05-08 20:24:53 +0800

    add extend method: findNotDeletedByCondition

---
+++ BootSocket/src/main/java/com/monkeybean/socket/core/ExtendAbstractService.java
@@ -0,0 +1,24 @@
+package com.monkeybean.socket.core;
+
+import tk.mybatis.mapper.entity.Condition;
+
+import java.util.List;
+
+/**
+ * Created by MonkeyBean on 2019/5/6.
+ */
+public abstract class ExtendAbstractService<T> extends AbstractService<T> {
+
+    /**
+     * 查找未删除的记录, 用于表中有is_deleted字段的数据处理
+     * 此方法低频使用, 实际多为联表查询, 在mapper.xml中手写sql更加便捷
+     */
+    public List<T> findNotDeletedByCondition(Condition condition) {
+        if(condition.getOredCriteria().isEmpty()){
+            condition.createCriteria().andEqualTo("is_deleted", false);
+        }else{
+            condition.getOredCriteria().get(0).andEqualTo("is_deleted", false);
+        }
+        return mapper.selectByCondition(condition);
+    }
+}
